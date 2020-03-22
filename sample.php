9579b8356fc07ebbcd098b55fbf03477c6e2cb81
rari.rari <raricom_com@yahoo.co.jp>
2020-03-22 07:42:08 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>おみくじ</title>
+</head>
+<body>
+    <h1>おみくじ</h1>
+    <p>おみくじの結果：無し</p>
+    <form action="sample.php" method="POST">
+        <button type="submit" name="act" value="draw">おみくじをひく！</button>
+    </form>
+</body>
+</html>
